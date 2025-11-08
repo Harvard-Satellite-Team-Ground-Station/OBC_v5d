a09089b38218e227d54f5b72bc8870f79a6c3c3d
Madison Davis <madisond2302@gmail.com>
2025-11-07 21:51:17 -0500

    ground station v2

--- src/ground-station/repl.py
+++ src/ground-station/repl.py
@@ -18,7 +18,9 @@ logger: Logger = Logger(
     error_counter=Counter(1),
     colorized=False,
 )
+
 config: Config = Config("config.json")
+
 jokes_config: JokesConfig = JokesConfig("jokes.json")
 
 # manually set the pin high to allow mcp to be detected
@@ -26,7 +28,6 @@ GPIO_RESET = (
     initialize_pin(logger, board.GPIO_EXPANDER_RESET, digitalio.Direction.OUTPUT, True),
 )
 
-# NEW
 spi0 = _spi_init(
     logger,
     board.SPI0_SCK,
@@ -67,7 +68,6 @@ sband_radio = SX1280Manager(
     initialize_pin(logger, board.RF2_TX_EN, digitalio.Direction.OUTPUT, False),
     initialize_pin(logger, board.RF2_RX_EN, digitalio.Direction.OUTPUT, False),
 )
-# END NEW
 
 """
 radio = RFM9xManager(
@@ -77,7 +77,7 @@ radio = RFM9xManager(
     initialize_pin(logger, board.SPI0_CS0, digitalio.Direction.OUTPUT, True),
     initialize_pin(logger, board.RF1_RST, digitalio.Direction.OUTPUT, True),
 )
-"""
+
 
 packet_manager = PacketManager(
     logger,
@@ -101,4 +101,34 @@ ground_station = GroundStation(
     cdh,
 )
 
-ground_station.run()
+# ground_station.run()
+"""
+
+import time
+def test_sband():
+    choice = input("Choose to be a receiver R or sender S.  Make sure the receiver starts up first!").strip().upper()
+    if choice == "R":
+        # Wait for any response
+        start_time = time.time()
+        while time.time() - start_time < 10:
+            message = sband_radio._radio.receive(keep_listening=False)
+            if message:
+                received = message
+                print("Received...", received)
+                break
+            else:
+                print("Still waiting...")
+            time.sleep(0.5)
+    else:
+        # Attempt to send the beacon
+        print("Sending in 2 seconds...")
+        time.sleep(2)
+        success = sband_radio._radio.send(b"Hello there from FCB!")
+        if not success:
+            print("❌ Beacon failed to send.")
+        else:
+            print("✅ Beacon sent successfully!")
+    print("Stopping test...")
+    return
+
+test_sband()
